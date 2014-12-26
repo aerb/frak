c70283166cdeb83005e5b13f12b16365e3d5c777
Adam Erb <adam.l.erb@gmail.com>
2014-12-26 16:56:56 +0100

    Grouping all handlers into map.

--- src/refac/core.clj
+++ src/refac/core.clj
@@ -3,24 +3,14 @@
   (use refac.io)
   (use refac.java))
 
-(def states
-  {"declaration"  handle-declaration
-   "base"         handle-base
-   "package"      handle-package
-   "import"       handle-import
-   "class"        handle-class
-   "type"         handle-type
-   "field"        handle-field-name
-   "class-body"   handle-class-body})
-
 (defn state-for [arg]
-  (if (contains? states arg)
-    (states arg)
+  (if (contains? handlers arg)
+    (handlers arg)
     (throw (Exception. arg))))
 
 (defn handle-symbol [context symbol]
-  (let [state   (context :state)
-        current (state :current)
+  (let [state   (:state context)
+        current (:current state)
         handler (state-for current)
         next    (handler context symbol)]
     next))
@@ -28,8 +18,17 @@
 (defn initial-context [] {:state {:current "base" :arg nil}
                           :class {:fields {}}})
 
+(defn fsm-reduce [reducer init items]
+  (loop [reduction init
+         items     items]
+    (if (not (empty? items))
+             (recur (reducer reduction
+                             (first items))
+                    (rest items)))))
+
 (defn process-symbols [symbols]
-  (reduce handle-symbol (initial-context) symbols))
+  (fsm-reduce handle-symbol (initial-context) symbols))
+
 
 (defn -main
   [& args]
--- src/refac/java.clj
+++ src/refac/java.clj
@@ -9,49 +9,51 @@
              (list symbol)
              (cons symbol namespace))]))
 
-(defn handle-package [context symbol]
-  (let [[complete namespace] (handle-namespace (get-in context [:class :namespace]) symbol)]
-    (if complete
-      (-> context
-          (goto-state "base")
-          (assoc-in [:class :namespace] namespace))
-      (assoc-in context [:class :namespace] namespace))))
-
-(defn handle-import [context symbol]
-  (if (= ";" symbol)
-    (goto-state context "base")
-    context))
-
-(defn handle-class [context symbol]
-  (case symbol
-    "{" (-> context
-            (goto-state "class-body"))
-    (-> context
-        (assoc-in [:class :name] symbol)
-        (assoc-in [:class :visability] (get-in context [:state :arg]))
-        (assoc-in [:state :arg] nil))))
-
-(defn handle-declaration [context symbol]
-  (case symbol
-    "class" (-> context (goto-state "class"))
-    (-> context
-        (goto-state-with "type" #(assoc % :type symbol)))))
-
-(defn handle-class-body [context symbol]
-  (case symbol
-    "}" (-> context (goto-state "base"))
-    (-> context (goto-state-with "declaration" {:visability symbol}))))
-
-(defn handle-field-name [context symbol]
-  (-> context
-      (goto-state "field")))
-
-(defn handle-type [context symbol]
-    (-> context
-        (goto-state-with "field" #(assoc % :name symbol))))
-
-(defn handle-base [context symbol]
-  (case symbol
-    ("class" "import" "package")                (goto-state context symbol)
-    ("private" "public" "protected" "internal") (goto-state-with context "declaration" symbol)
-    (throw IllegalStateException)))
+(def handlers {
+   "package"
+    (fn [context symbol]
+      (let [[complete namespace] (handle-namespace (get-in context [:class :namespace]) symbol)]
+        (if complete
+          (-> context
+              (goto-state "base")
+              (assoc-in [:class :namespace] namespace))
+          (assoc-in context [:class :namespace] namespace))))
+   "import"
+   (fn [context symbol]
+     (if (= ";" symbol)
+       (goto-state context "base")
+       context))
+   "class"
+   (fn [context symbol]
+     (case symbol
+       "{" (-> context
+               (goto-state "class-body"))
+       (-> context
+           (assoc-in [:class :name] symbol)
+           (assoc-in [:class :visability] (get-in context [:state :arg])))))
+   "declaration"
+   (fn [context symbol]
+     (case symbol
+       "class" (-> context (goto-state "class"))
+       (-> context
+           (goto-state-with "type" #(assoc % :type symbol)))))
+   "class-body"
+   (fn [context symbol]
+     (case symbol
+       "}" (-> context (goto-state "base"))
+       (-> context (goto-state-with "declaration" {:visability symbol}))))
+   "field"
+   (fn [context symbol]
+     (-> context
+         (goto-state "field")))
+   "type"
+   (fn [context symbol]
+     (-> context
+         (goto-state-with "field" #(assoc % :name symbol))))
+   "base"
+   (fn [context symbol]
+     (case symbol
+       ("class" "import" "package")                (goto-state context symbol)
+       ("private" "public" "protected" "internal") (goto-state-with context "declaration" symbol)
+       (throw IllegalStateException)))
+   })
